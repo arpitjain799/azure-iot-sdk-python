baf926d4af13736f7e0ea0ef1d11a87f125cf419
Bert Kleewein <bertk@micosoft.com>
2023-04-19 15:14:33 -0700

    Passing

--- tests/e2e/edge_setup/deploy/make-edge-manifest.sh
+++ tests/e2e/edge_setup/deploy/make-edge-manifest.sh
@@ -23,22 +23,6 @@ PATH_HUB_PROPS=".modulesContent[\"\$edgeHub\"][\"properties.desired\"]"
 PATH_ROUTES="${PATH_HUB_PROPS}.routes"
 
 #
-# JSON for empty module block inside the deployment manifest
-#
-read -d '' EMPTY_MODULE_BLOCK << EOF
-{
-  "version": "1.0",
-  "type": "docker",
-  "status": "running",
-  "restartPolicy": "always",
-  "settings": {
-    "image": "",
-    "createOptions": {}
-  }
-}
-EOF
-
-#
 # JSON for container retistry credentials
 #
 read -d '' REGISTRY_BLOCK << EOF
@@ -52,42 +36,12 @@ read -d '' REGISTRY_BLOCK << EOF
 EOF
 
 #
-# function to format a route from a module output to a module input
-#
-function output_to_input_route() {
-    OUTPUT_MOD=$1
-    OUTPUT_NAME=$2
-    INPUT_MOD=$3
-    INPUT_NAME=$4
-
-    # Quotes that end up in the final JSON need to be double-escaped (ie `\\\"`)
-    echo "FROM /messages/modules/${OUTPUT_MOD}/outputs/${OUTPUT_NAME} " \
-         "INTO BrokeredEndpoint(\\\\\"/modules/${INPUT_MOD}/inputs/${INPUT_NAME}\\\\\")"
-}
-
-#
-# JSON for the full set of message routes
-#
-read -d '' ROUTE_BLOCK << EOF
-{
-    test_to_echo: \"$(output_to_input_route testMod output1 echoMod input1)\",
-    echo_to_test: \"$(output_to_input_route echoMod output2 testMod input2)\",
-    default_route: \"FROM /messages/modules/testMod/* INTO \$upstream\"
-}
-EOF
-
-#
 # Use JQuery to populate the deployment manifest using the variables we set above.
 #
 cat ${script_dir}/deployment.template.json |\
     jq "${PATH_SYSTEM_MODULES}.edgeAgent.settings.image = \"${DOCKER_IMAGE_EDGE_AGENT}\"" |\
-    jq "${PATH_SYSTEM_MODULES}.edgeAgent.settings.createOptions |= tojson" |\
     jq "${PATH_SYSTEM_MODULES}.edgeHub.settings.image = \"${DOCKER_IMAGE_EDGE_HUB}\"" |\
-    jq "${PATH_SYSTEM_MODULES}.edgeHub.settings.createOptions |= tojson" |\
-    jq "${PATH_MODULES} = {}" |\
-    jq "${PATH_MODULES}.testMod = ${EMPTY_MODULE_BLOCK}" |\
     jq "${PATH_MODULES}.testMod.settings.image = \"${IOTHUB_E2E_REPO_ADDRESS}/default-friend-module:64-v2\"" |\
     jq "${PATH_REGISTRY_CREDENTIALS} = ${REGISTRY_BLOCK}" |\
-    jq "${PATH_ROUTES} = ${ROUTE_BLOCK}" |\
     jq "${PATH_MODULES}.testMod.settings.createOptions |= tojson"
 
