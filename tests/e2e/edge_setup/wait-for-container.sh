abb0ff5b231f2e60145bfe19ac6b18594b5515c8
Bert Kleewein <bertk@micosoft.com>
2023-04-20 14:20:49 -0700

    update check

--- tests/e2e/edge_setup/wait-for-container.sh
+++ tests/e2e/edge_setup/wait-for-container.sh
@@ -3,6 +3,7 @@ CONTAINER_NAME=$1
 while true; do
     state=$(docker inspect -f {{.State.Running}} ${CONTAINER_NAME})
     if [ $? -eq 0 ] && [ "${state}" == "true" ]; then
+        echo ${CONTAINER_NAME} is running
         exit 0
     else
         sleep 5
